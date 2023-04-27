54a744ce5259f8e3bf3f9b6ea537353a8cc050c2
Rai-vaz <ray.vaz04@gmail.com>
2023-04-27 15:41:53 -0300

    Início efeito parallax

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './components/About';
 import Services from './components/Services';
 import Dados from './components/Dados'
 import CircularProgress from './components/CircularProgress';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   const image = document.getElementsByClassName('thumbnail');
@@ -22,13 +22,25 @@ function App() {
       <Slider/>
       <About/>
       <Services/>
-      <Dados/>
-      <Container>
-        <CircularProgress value={450}/>
-        <CircularProgress value={200}/>
-        <CircularProgress value={150}/>
-        <CircularProgress value={120}/>
-      </Container>
+        <div className='data-area'>
+          <Container fluid={true}>
+              <Row>
+                <Col md={3} sm={6} className='progress-box'>
+                  <CircularProgress value={450} lable={'Clientes'}/>
+                </Col>
+                <Col md={3} sm={6} className='progress-box'>
+                  <CircularProgress value={200}/>
+                </Col>
+                <Col  md={3} sm={6} className='progress-box'>
+                  <CircularProgress value={150}/>
+                </Col>
+                <Col md={3} sm={6} className='progress-box'>
+                  <CircularProgress value={120}/>
+                </Col>
+              </Row>
+          </Container>
+        </div>
+      <div id='x'></div>
       
     </div>
     
--- src/components/CircularProgress.jsx
+++ src/components/CircularProgress.jsx
@@ -10,34 +10,34 @@ import {useRef, useState} from 'react'
 {/*imports css do componente*/}
 import '../css/CircularProgressbar.css'
 
+import {Row, Col} from 'react-bootstrap'
 
-const CircularProgress = ({value}) => {
+const CircularProgress = ({value, lable}) => {
   const initialValue = 1
   const container_circle_progress_ref = useRef('')
   const [loadCircularProgress, setLoadCircularProgress] = useState(null)
   
   const handleChange = (isVisible) => {
-    console.log(isVisible)
     setLoadCircularProgress(isVisible)
   }
-  
+
   return (
     <VisibilitySensor onChange={handleChange} partialVisibility ref={container_circle_progress_ref}>
-      <div className='progress-bar'>
+      <div className='circular-progress-wrapper'>
         <CircularProgressbar
           minValue={0}
           maxValue={value}
           value={loadCircularProgress ? value : initialValue}
-          text={loadCircularProgress ? value : initialValue}
+          text={value}
           styles={buildStyles({
             pathTransitionDuration: 1,
             strokeLinecap: 'round',
             pathColor: 'rgb(62, 152, 199)'
           })}
         />
+        <p className='label'>{lable}</p>
       </div>
     </VisibilitySensor>
-    
   );
   
 }
